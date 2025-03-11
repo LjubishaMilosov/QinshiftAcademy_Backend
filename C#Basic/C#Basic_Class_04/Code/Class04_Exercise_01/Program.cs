6d8962fa7aeaa087fe22d42ee3546412c66b5dbc
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-12 03:49:31 +0800

    Methods

---
+++ C#Basic/C#Basic_Class_04/Code/Class04_Exercise_01/Program.cs
@@ -0,0 +1,41 @@
+﻿int Sum(int a, int b)
+{
+    return a + b;
+}
+
+int Substract(int a, int b)
+{
+    return a - b;
+}
+
+
+Console.WriteLine("Enter the first number:");
+bool firstSucces = int.TryParse(Console.ReadLine(), out int firstNumber);
+
+Console.WriteLine("Enter the second number:");
+bool secondSucces = int.TryParse(Console.ReadLine(), out int secondNumber);
+
+Console.WriteLine("Enter + or -");
+string op = Console.ReadLine(); 
+
+if(firstSucces && secondSucces)
+{
+    if(op == "+")
+    {
+        int sum = Sum(firstNumber, secondNumber);
+        Console.WriteLine("The sum is: " + sum);
+    }
+    else if(op == "-")
+    {
+        int substract = Substract(firstNumber, secondNumber);
+        Console.WriteLine("The substract is: " + substract);
+    }
+    else
+    {
+        Console.WriteLine("Invalid operator");
+    }
+}
+else
+{
+    Console.WriteLine("Invalid input");
+}
