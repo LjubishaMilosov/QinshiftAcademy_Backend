fbddbb94071a56be6a67be5e2b6247f634be8b2e
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-12 03:46:01 +0800

    Class04_Exercise01

--- C#Basic/C#Basic_Class_04/Code/Class03_Exercise06/Program.cs
+++ C#Basic/C#Basic_Class_04/Code/Class03_Exercise06/Program.cs
@@ -5,27 +5,47 @@
 //Do the same process over and over until the user enters N
 //Print all the names after user enters N
 //
-string[] names = new string[0];  // empty array -> 0 represents the number of elments
+//string[] names = new string[0];  // empty array -> 0 represents the number of elments
 //string names2 = new string[] { };  // empty array
 
-int i = 0;
+//int i = 0;
 
-while(true)
+//while(true)
+//{
+//    Console.WriteLine("Enter a name: ");
+//    string name = Console.ReadLine();
+
+//    // we need to change the size of our array
+//    Array.Resize(ref names, names.Length + 1);  // we are increasing the size of the array by 1
+//    names[i] = name;
+//    i++;
+
+//    // names[names.Length - 1] = name;  // -. another way- withouth using counter: names.length - 1 is always the index of the last element
+
+//    Console.WriteLine("Do you want to enter asnother name (Y/N?)");
+//    string input = Console.ReadLine();
+//    if(input == "n" || input == "N")
+//    {
+//        break;
+//    }   
+//}
+
+
+string[] names = new string[0];  // empty array -> 0 represents the number of elements
+int i = 0;
+string input;
+
+do
 {
     Console.WriteLine("Enter a name: ");
     string name = Console.ReadLine();
 
-    // we need to change the size of our array
     Array.Resize(ref names, names.Length + 1);  // we are increasing the size of the array by 1
-    names[1] = name;
+    names[i] = name;
     i++;
 
-    // names[names.Length - 1] = name;  // -. another way- withouth using counter: names.length - 1 is always the index of the last element
-
-    Console.WriteLine("Do you want to enter asnother name (Y/N?)");
-    string input = Console.ReadLine();
-    if(input == "n" || input == "N")
-    {
-        break;
-    }   
+    Console.WriteLine("Do you want to enter another name (Y/N?)");
+    input = Console.ReadLine();
 }
+while (input != "n" && input != "N");
+
