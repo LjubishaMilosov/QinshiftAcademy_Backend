343bb23b08f4f840aa1dcaf6d56e0b873f4a379b
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-16 01:33:12 +0800

    exercise01_class06

---
+++ C#Basic/C#Basic_Class_06/Code/Exercise_01/Human.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_01
+{
+    class Human 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Human(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+
+        public string GetPersonStats()
+        {
+            return $"{FirstName} {LastName} is {Age} years old.";
+        }
+    }
+}
---
+++ C#Basic/C#Basic_Class_06/Code/Exercise_01/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
+using Exercise_01;
+
+//EXERCISE 1
+//● Create a class Human
+//● Add properties: FirstName, LastName, Age
+//● Create a method called GetPersonStats that returns the
+//full name of the human as well as their age
+//● Create an object human by asking the user to fill the
+//required information
+//● Call the GetPersonStats method and print the result in
+//the console after the object is created
+
+Console.WriteLine("Please enter your first name:");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Please enter your last name:");
+string lastName = Console.ReadLine();
+
+Console.WriteLine("Please enter your age:");
+int age = int.Parse(Console.ReadLine());
+
+Human human = new Human(firstName, lastName, age);
+Console.WriteLine(human.GetPersonStats());
