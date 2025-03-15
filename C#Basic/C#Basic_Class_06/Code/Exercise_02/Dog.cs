9e3f7c7a18fc0f454eb8087d1da5c91ed77fcc7a
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-16 02:45:14 +0800

    class06_exercise02

---
+++ C#Basic/C#Basic_Class_06/Code/Exercise_02/Dog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+//Create a class Dog
+//● Add properties: Name, Race, Color
+//● The dog needs to have an Eat method that returns
+//message: The dog is now eating. A Play method
+//returning a message : The dog is now playing. and a
+//ChaseTail method that returns a message: Dog is now
+//chasing its tail.
+//● The user needs to create the dog object by inputs and
+//then give an option to choose one of the actions
+//mentioned above.
+//EXERCISE 2
+
+namespace Exercise_02
+{
+    class Dog
+    {
+        public string Name { get; set; }
+        public string Race { get; set; }
+        public string Color { get; set; }
+
+        public Dog() // empty default constructor
+        {
+        }
+
+        public Dog(string name, string race, string color)
+        {
+            Name = name;
+            Race = race;
+            Color = color;
+        }
+
+        public string Eat()
+        {
+            return $"{Name} is now eating.";
+        }
+        public string Play()
+        {
+            return $"{Name} is now playing.";
+        }
+        public string ChaseTail()
+        {
+            return $"{Name} is now chasing its tail.";
+        }
+    }
+}
---
+++ C#Basic/C#Basic_Class_06/Code/Exercise_02/Program.cs
@@ -0,0 +1,79 @@
+﻿
+using Exercise_02;
+
+//  Dog dog1 = new Dog();
+
+Console.WriteLine("Enter the dog's name: ");
+string name = Console.ReadLine();
+//  dog1.Name = Console.ReadLine();
+
+Console.WriteLine("Enter the dog's race: ");    
+string race = Console.ReadLine();
+
+// dog1.Race = Console.ReadLine();
+
+Console.WriteLine("Enter the dog's color: ");
+string color = Console.ReadLine();
+
+// dog1.Color = Console.ReadLine();
+
+Dog dog = new Dog(name, race, color);
+
+Console.WriteLine("Choose an action: \n 1. Feed the dog \n 2. Play with the dog \n 3. Chase its tail");
+
+void DogOptions(Dog dog, int option)
+{
+    switch (option)
+    {
+        case 1:
+            Console.WriteLine(dog.Eat());
+            break;
+        case 2:
+            Console.WriteLine(dog.Play());
+            break;
+        case 3:
+            Console.WriteLine(dog.ChaseTail());
+            break;
+        default:
+            Console.WriteLine("Invalid choice. Please select 1, 2, or 3.");
+            break;
+    }
+}
+
+int result; // we need to declare it outside the while lop to be able to use it
+while (!int.TryParse(Console.ReadLine(), out result)) //We initialize  result in the while loop condition
+{
+    Console.WriteLine("You have entered a wrong option");
+    Console.WriteLine("Choose an action: \n 1. Feed the dog \n 2. Play with the dog \n 3. Chase its tail");
+}
+
+DogOptions(dog, result);
+
+
+//string choice;
+//bool validChoice = false;
+
+//while (!validChoice)
+//{
+//    Console.WriteLine("Choose an action: \n 1. Feed the dog \n 2. Play with the dog \n 3. Chase its tail");
+//    choice = Console.ReadLine();
+
+//    switch (choice)
+//    {
+//        case "1":
+//            Console.WriteLine(dog.Eat());
+//            validChoice = true;
+//            break;
+//        case "2":
+//            Console.WriteLine(dog.Play());
+//            validChoice = true;
+//            break;
+//        case "3":
+//            Console.WriteLine(dog.ChaseTail());
+//            validChoice = true;
+//            break;
+//        default:
+//            Console.WriteLine("Invalid choice. Please select 1, 2, or 3.");
+//            break;
+//    }
+//}
