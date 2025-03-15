23cc58bda2bfb014c7988e7375a7b509fb8e5cd0
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-16 04:19:35 +0800

    class06_exercise03

---
+++ C#Basic/C#Basic_Class_06/Code/Exercise_03/Program.cs
@@ -0,0 +1,36 @@
+﻿using Exercise_03;
+
+Student[] students = new Student[]
+{
+    new Student("Sinisha", "Qinshift", "G6"),
+    new Student("Viktor", "Qinshift", "G6"),
+    new Student("Ivan", "Qinshift", "G6"),
+    new Student("Olgica", "Qinshift", "G6"),
+    new Student("Martin", "Qinshift", "G6")
+};
+
+Console.WriteLine("Please enter a student name:");
+string name = Console.ReadLine();
+
+Student student = FindStudent(students, name);
+
+if (student != null)
+{
+    student.PrintIfo();
+}
+else
+{
+    Console.WriteLine("There is no student with that name");
+}
+
+Student FindStudent(Student[] studentArray, string name)
+{
+    foreach (Student student in studentArray)
+    {
+        if (student.Name.ToLower() == name.ToLower())
+        {
+            return student;
+        }
+    }
+    return null; // after we iterae the whole array, if there was no match(did not go in the if statement) we return null
+}
---
+++ C#Basic/C#Basic_Class_06/Code/Exercise_03/Student.cs
@@ -0,0 +1,33 @@
+﻿
+//Create a class Student that has properties: Name,
+//Academy and Group
+//● Create an array with 5 new Students(objects)
+//● The user should enter a name and the user information
+//should be displayed in the console if a user by that
+//name exists
+//● If there is no such user it should show an error
+//message
+
+
+namespace Exercise_03
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public string Academy { get; set; }
+        public string Group { get; set; }
+
+        public Student(string name, string academy, string group)
+        {
+            Name = name;
+            Academy = academy;
+            Group = group;
+        }
+        public void PrintIfo()
+        {
+            Console.WriteLine($"Name: {Name}, Academy: {Academy}, Group: {Group}");
+        }
+
+
+    }
+}
