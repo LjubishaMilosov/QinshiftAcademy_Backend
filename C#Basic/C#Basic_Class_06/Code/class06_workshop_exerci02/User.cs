9eac77cb2054378dc8ff1c3fcc4045f61932d273
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-17 02:30:25 +0800

    class06_workshop_exercise02

---
+++ C#Basic/C#Basic_Class_06/Code/class06_workshop_exerci02/User.cs
@@ -0,0 +1,18 @@
+﻿//Create a class User with the following:
+//Id - int
+//Username - string
+//Password - string
+//Messages - Array of strings Create an array of users and add 3 users to it manually ( hard-coded ).
+namespace class06_workshop_exerci02
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string[] Messages { get; set; }
+        
+        
+
+    }
+}
