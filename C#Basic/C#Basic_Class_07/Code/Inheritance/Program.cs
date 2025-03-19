8aa475e0568575486958ac5394ecc8711fac7e2b
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-20 03:50:06 +0800

    Inheritance-part1

---
+++ C#Basic/C#Basic_Class_07/Code/Inheritance/Program.cs
@@ -0,0 +1,11 @@
+﻿using Inheritance.Models;
+
+Animal animal = new Animal();
+animal.Name = "test animal";
+animal.Type = "Test type";
+//animal.Age => ERROR => protected property (
+// animal.id => ERROR => private property
+
+animal.PrintInfo();
+
+// animal.PrintAge(); ==> ERORR => does not exist in Animal, only in Cat class
