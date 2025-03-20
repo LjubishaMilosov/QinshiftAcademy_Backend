a8a856c2b4e0a423e8e2c12139f006687ed2fa5c
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-21 01:26:20 +0800

    enums-part1

---
+++ C#Basic/C#Basic_Class_07/Code/Enums/DayOfWeekEnum.cs
@@ -0,0 +1,14 @@
+﻿namespace Enums
+{
+    public enum DayOfWeekEnum  //we change the name of our enum and we usuall add Enum at the end of the name
+    {
+        //if we don't specify the value of the first enum, we will have value zero
+        Monday = 1,
+        Tuesday, // the next value will have the previous enum value + 1
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
---
+++ C#Basic/C#Basic_Class_07/Code/Enums/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using Enums;
+
+int day = 5;
+
+if(day == 5) // not a good practice, not clear
+{
+    Console.WriteLine();
+}
+
+if(day == (int)DayOfWeekEnum.Friday) // we need to convert(cast, get the value from the enum) the enum to int so that we can compare it to another int
+{
+    Console.WriteLine("Yaay it is Friday");
+}
+
+
