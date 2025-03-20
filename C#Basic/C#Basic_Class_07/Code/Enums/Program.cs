edd2abe116c1fd1ae17cbcdd3e2bccfa49988438
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-21 01:27:33 +0800

    enums-part2

--- C#Basic/C#Basic_Class_07/Code/Enums/Program.cs
+++ C#Basic/C#Basic_Class_07/Code/Enums/Program.cs
@@ -14,3 +14,19 @@ if(day == (int)DayOfWeekEnum.Friday) // we need to convert(cast, get the value f
 }
 
 
+int roleNumber = 1;
+
+if (roleNumber == (int)RoleEnum.Administrator)
+{
+    Console.WriteLine($"This is the {RoleEnum.Administrator.ToString()}");
+}
+else if (roleNumber == (int)RoleEnum.User)
+{
+    Console.WriteLine($"This is the {RoleEnum.User.ToString()}");
+}
+
+
+// (int)RoleEnum.Administrator;           // this way we get the value (number value) of the enum
+// RoleEnum.Administrator.ToString();     // this way we get the name of the enum  
+
+Console.ReadLine();
