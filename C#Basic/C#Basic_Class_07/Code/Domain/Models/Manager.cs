b4626e35d64b517ee8fd3d014c30c6b55a45ebfc
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-21 01:30:23 +0800

    class_library-part2

---
+++ C#Basic/C#Basic_Class_07/Code/Domain/Models/Manager.cs
@@ -0,0 +1,37 @@
+﻿//Add class Manager that inherits from Employee and
+//has a new property called Bonus - double, private
+//● Create a constructor that sets all properties except
+//Bonus
+//● Create a method called AddBonus that adds bonus to
+//the Bonus property
+//● Override the method GetSalary to return the Salary +
+//Bonus
+//EXERCISE 3
+
+
+using Domain.Enums;
+
+namespace Domain.Models
+{
+    public class Manager : Employee
+    {
+        private double _Bonus { get; set; }
+
+        public Manager(string firstname, string lastName, double salary)
+                : base(firstname, lastName, RoleEnum.Manager, salary)
+        {
+            _Bonus = 0;
+        }
+
+        public void AddBonus(double bonus)
+        {
+            _Bonus += bonus;
+        }
+
+        public override double GetSalary()
+        {
+            return Salary + _Bonus;
+            // return base.GetSalary() + _Bonus;
+        }
+    }
+}
