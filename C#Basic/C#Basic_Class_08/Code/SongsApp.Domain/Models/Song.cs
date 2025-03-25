e15b7abfab6c11c7e81aee21593945423dec43dd
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-25 19:45:23 +0800

    songApp_song_class

---
+++ C#Basic/C#Basic_Class_08/Code/SongsApp.Domain/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿
+using SongsApp.Domain.Enums;
+
+namespace SongsApp.Domain.Models
+{
+    public class Song
+    {
+        public string Title { get; set; }
+        public double Length { get; set; }
+        public GenreEnum Genre { get; set; }
+
+        public Song(string Title, double length, GenreEnum genre)
+        {
+            //TODO - validations
+            this.Title = Title;
+            this.Length = length;
+            this.Genre = genre;
+        }
+    }
+}
