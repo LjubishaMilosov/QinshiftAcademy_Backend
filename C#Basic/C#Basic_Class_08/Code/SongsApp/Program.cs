4bce030a49e487b053f47d1328f8f5ed9f4fcebc
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-25 19:46:06 +0800

    songApp_consoleApp

---
+++ C#Basic/C#Basic_Class_08/Code/SongsApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using SongsApp.Domain.Enums;
+using SongsApp.Domain.Models;
+
+Person person = new Person("Petko", "Petkovski", 25, GenreEnum.Rock);
+person.GetFavSongs();
+
+person.FavoriteSongs.Add(new Song("Stairway to Heaven", 8.02, GenreEnum.Rock));
+person.FavoriteSongs.Add(new Song("Bohemian Rhapsody", 5.55, GenreEnum.Rock));
+
+person.GetFavSongs();
