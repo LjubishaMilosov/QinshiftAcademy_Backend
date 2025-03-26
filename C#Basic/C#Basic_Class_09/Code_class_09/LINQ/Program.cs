05ff5b4099bfefb164eda59e07157e5bc2b033c6
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-26 21:36:41 +0800

    LINQ methods First()/FirstOrDefaullt()

--- C#Basic/C#Basic_Class_09/Code_class_09/LINQ/Program.cs
+++ C#Basic/C#Basic_Class_09/Code_class_09/LINQ/Program.cs
@@ -23,3 +23,41 @@ foreach (Dog dog in dogsWithNameLongerThan4)
 {
     Console.WriteLine(dog.Name);
 }
+
+// all dogs whose name starts with L 
+List<Dog> dogsStartingWithL = dogs.Where(d => d.Name.StartsWith("L") || d.Name.StartsWith("l")).ToList();
+
+
+List<Dog> dogsDoesNotStartingWithL = dogs.Where(d => !d.Name.StartsWith("L") && !d.Name.StartsWith("l")).ToList();
+
+Console.WriteLine("============Starts with L ===============");
+foreach(Dog dog in dogsStartingWithL)
+{
+    Console.WriteLine(dog.Name);
+}
+
+Console.WriteLine("============DOes Not Start with L ===============");
+foreach (Dog dog in dogsDoesNotStartingWithL)
+{
+    Console.WriteLine(dog.Name);
+}
+
+
+// Find the first dog older than two
+//First Way
+Dog firstDogOlderThanTwo1 = dogs.Where(d => d.Age > 2).FirstOrDefault(); // gets all dogs older than 2 and then returns the first one
+
+// Second Way
+Dog firstDogOlderThanTwo2 = dogs.FirstOrDefault(d => d.Age > 2); // gets the firt dog older than 2
+
+//  Dog dogWithNameOnA = dogs.First(d => d.Name.StartsWith("A")); // this will throw an error: Secuence contains no matching element 
+
+Dog dogWithNameOnA = dogs.FirstOrDefault(d => d.Name.StartsWith("A"));
+if(dogWithNameOnA == null)
+{
+    Console.WriteLine("No dog with name starting with A");
+}
+else
+{
+    Console.WriteLine(dogWithNameOnA.Name);
+}
