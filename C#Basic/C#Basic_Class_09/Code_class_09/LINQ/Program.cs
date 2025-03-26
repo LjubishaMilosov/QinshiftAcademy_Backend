1bfc30d8c545194262a10b9abc6d9165ab3443c9
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-26 21:20:59 +0800

    LINQ-where method

--- C#Basic/C#Basic_Class_09/Code_class_09/LINQ/Program.cs
+++ C#Basic/C#Basic_Class_09/Code_class_09/LINQ/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LINQ;
+
+List<Dog> dogs = new List<Dog>()
+{
+    new Dog("Sparky", 1),
+    new Dog("Butch", 2),
+    new Dog("Hera", 3),
+    new Dog("Luna", 1),
+    new Dog("Lucy", 2),
+    new Dog("Buck", 4),
+};
+
+// Find all dogs whose name has more tha 4 chaacters
+// thi does not change the original list
+List<Dog> dogsWithNameLongerThan4 = dogs.Where(x => x.Name.Length > 4).ToList();
+Console.WriteLine("============ORIGINAL LIST===============");
+foreach (Dog dog in dogs)
+{
+    Console.WriteLine(dog.Name);
+}
+Console.WriteLine("============LONGER THAN 4 ===============");
+foreach (Dog dog in dogsWithNameLongerThan4)
+{
+    Console.WriteLine(dog.Name);
+}
