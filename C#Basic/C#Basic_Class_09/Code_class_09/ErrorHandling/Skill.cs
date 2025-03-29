4aa39965969a8eaaed6b496fd5c923276f14b33f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-30 02:14:33 +0800

    class Skill

---
+++ C#Basic/C#Basic_Class_09/Code_class_09/ErrorHandling/Skill.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ErrorHandling
+{
+    public class Skill
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+
+        public Skill(string name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+    }
+}
