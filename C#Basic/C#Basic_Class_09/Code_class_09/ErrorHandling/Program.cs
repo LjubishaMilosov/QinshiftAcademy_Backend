898b79a81ede31e335cc5b1a9589e8980ab4fe3d
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-03-30 02:23:36 +0800

    try catch ErrorHandling

---
+++ C#Basic/C#Basic_Class_09/Code_class_09/ErrorHandling/Program.cs
@@ -0,0 +1,23 @@
+﻿try
+{
+    // in try block we write the code that we want to be ecuted and that might throw an error
+    Console.WriteLine("Enter a number: ");
+    string input = Console.ReadLine();
+
+    int number = int.Parse(input); //if we enter a wrong input, this line will throw an exception
+
+    // this line will be executed only if the lines above don't throw an error
+    // otherwise we will be redirected to the catch block
+    Console.WriteLine(number);
+}
+catch(Exception ex)
+{
+    //the catch block will be executed only if an exception is thrown in the try block
+    Console.WriteLine("The input you entered was not a correct number");
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    // the finally block will be executed no matter what
+    Console.WriteLine("This is the finally block");
+}
