0ee4d545df9a1ee48b60e8b465a8999b8cdfc002
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-01 03:07:00 +0800

    movie class

---
+++ C#Basic/C#Basic_Class_10/Code/MoviesApp.Domain/Models/Movie.cs
@@ -0,0 +1,29 @@
+﻿
+using MoviesApp.Domain.Enums;
+
+namespace MoviesApp.Domain.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Rating { get; set; }
+        public int TicketPrice { get; set; }
+        public Movie(string title, Genre genre, int rating)
+        {
+            if(string .IsNullOrEmpty(title))
+            {
+                throw new ArgumentNullException("Title must have a value");
+            }
+            Title = title;
+            Genre = genre;
+
+            if(rating < 1 || rating > 5)
+            {
+                throw new InvalidDataException("Rating must be a value between 0 and 5");
+            }
+            Rating = rating;
+            TicketPrice = 5 * rating;
+        }
+    }
+}
