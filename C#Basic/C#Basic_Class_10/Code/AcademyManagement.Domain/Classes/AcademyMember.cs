9064c3211c287d7a9e12c5bccf443b1be115ff9c
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-02 15:10:19 +0800

    academyMember class

---
+++ C#Basic/C#Basic_Class_10/Code/AcademyManagement.Domain/Classes/AcademyMember.cs
@@ -0,0 +1,23 @@
+﻿
+using AcademyManagement.Domain.Enums;
+
+namespace AcademyManagement.Domain.Classes
+{
+    public class AcademyMember
+    {
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Role Role { get; set; }
+
+        public AcademyMember(string username, string firstName, string lastName, int age, Role role) 
+        {
+            UserName = username;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Role = role;
+        }
+    }
+}
