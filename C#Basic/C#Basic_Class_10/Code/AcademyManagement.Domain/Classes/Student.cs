bdb32edde33ee3c91ec8593dbb1c79cb2105ea4f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-02 15:12:04 +0800

    student class

---
+++ C#Basic/C#Basic_Class_10/Code/AcademyManagement.Domain/Classes/Student.cs
@@ -0,0 +1,12 @@
+﻿using AcademyManagement.Domain.Enums;
+namespace AcademyManagement.Domain.Classes
+{
+    public class Student : AcademyMember
+    {
+        public Subject CurrentSubject { get; set; }
+        public Dictionary<Subject, int> Grades { get; set; }
+
+        public Student(string username, string firstName, string lastName, int age)
+            : base(username, firstName, lastName, age, Role.Student) { }
+    }
+}
