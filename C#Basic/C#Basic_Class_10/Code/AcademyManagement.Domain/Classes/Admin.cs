bcdb2a9b0721a69c0f28eca2d1a410ebcf7b90a3
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-02 15:11:12 +0800

    admin class

---
+++ C#Basic/C#Basic_Class_10/Code/AcademyManagement.Domain/Classes/Admin.cs
@@ -0,0 +1,11 @@
+﻿using AcademyManagement.Domain.Enums;
+
+namespace AcademyManagement.Domain.Classes
+{
+    public class Admin : AcademyMember
+    {
+        
+        public Admin(string username, string firstName, string lastName, int age)
+            : base(username, firstName, lastName, age, Role.Admin) { }
+    }
+}
