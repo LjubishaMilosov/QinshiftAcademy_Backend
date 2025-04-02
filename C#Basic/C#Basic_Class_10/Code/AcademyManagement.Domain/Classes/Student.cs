759f06983ebee614c6570f13c1c92f24e8742103
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-03 02:58:58 +0800

    student class complete

--- C#Basic/C#Basic_Class_10/Code/AcademyManagement.Domain/Classes/Student.cs
+++ C#Basic/C#Basic_Class_10/Code/AcademyManagement.Domain/Classes/Student.cs
@@ -8,5 +8,35 @@ namespace AcademyManagement.Domain.Classes
 
         public Student(string username, string firstName, string lastName, int age)
             : base(username, firstName, lastName, age, Role.Student) { }
+
+        public string GetFullName()
+        {
+            return $"{FirstName} {LastName}";
+        }
+        public void PrintDetails()
+        {
+            Console.WriteLine($"{FirstName} {LastName} {Age}");
+            if (CurrentSubject == null)
+            {
+                Console.WriteLine("The student is currently on a holiday");
+            }
+            else
+            {
+                Console.WriteLine($"The current subject is {CurrentSubject.Name}");
+            }
+            if (Grades == null)
+            {
+                Console.WriteLine("The student has still no grades");
+            }
+            else
+            {
+                Console.WriteLine("Grades:");
+                foreach (var item in Grades)
+                {
+                    Console.WriteLine($"{item.Key.Name} {item.Value}"); // item.Key == subject, item.Value == grade (int)
+
+                }
+            }
+        }
     }
 }
