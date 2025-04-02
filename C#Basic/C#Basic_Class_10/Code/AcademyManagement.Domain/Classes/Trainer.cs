01cb3c15e7a4d8ab29b6b267925aced8bc4dc2ee
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-02 15:11:32 +0800

    trainer class

---
+++ C#Basic/C#Basic_Class_10/Code/AcademyManagement.Domain/Classes/Trainer.cs
@@ -0,0 +1,9 @@
+﻿using AcademyManagement.Domain.Enums;
+namespace AcademyManagement.Domain.Classes
+{
+    public class Trainer : AcademyMember
+    {
+        public Trainer(string username, string firstName, string lastName, int age)
+            : base(username, firstName, lastName, age, Role.Trainer) { }
+    }
+}
