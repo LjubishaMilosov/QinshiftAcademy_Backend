121e749b0622d6ec4d89e691e12c40efb314afc0
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-25 00:34:39 +0800

    class01Task01 new project

---
+++ C#Advanced/C#_Advanced_Class01/Code/Task)1.Logic/Models/NameCountResult.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Task_1.Logic.Models
+{
+    public class NameCountResult
+    {
+        public string Name { get; set; }
+        public int Count { get; set; }
+        public NameCountResult(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+    }
+}
