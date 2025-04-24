d5f207d00cd02d5d87be34bd6ac6837216b1f2b5
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-25 00:35:06 +0800

    task01TextService

---
+++ C#Advanced/C#_Advanced_Class01/Code/Task)1.Logic/Services/TextService.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Task_1.Logic.Models;
+
+namespace Task_1.Logic.Services
+{
+    public class TextService
+    {
+        public List<NameCountResult> CountNameAppearancesInText(string text, List<string> names)
+        {
+           var nwordsInText  = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            var nameCountResults = new List<NameCountResult>();
+            foreach (var name in names)
+            {
+                var count = nwordsInText.Count(word => string.Equals(word, name, StringComparison.OrdinalIgnoreCase));
+                nameCountResults.Add(new NameCountResult(name, count));
+            }
+            return nameCountResults;
+        }
+    }
+}
