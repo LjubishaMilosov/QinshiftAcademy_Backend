9abfd5db91a6d6fe87171ef73a8c6a3a95eefed2
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-04-30 21:34:45 +0800

    abstractClassesAndInterfaces-developer class

---
+++ C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces.Domain/Models/Developer.cs
@@ -0,0 +1,27 @@
+﻿
+namespace AbstractClassesAndInterfaces.Domain.Models
+{
+    public class Developer : Person
+    {
+        public string ProjectName { get; set; }
+        public int YearsOfExperience { get; set; }
+        public List<string> ProgrammingLanguages { get; set; }
+
+        public Developer() { }
+
+        public Developer(string fullName, int age, string address, long phoneNumber,
+            string projectName, int yearsOfExperience, List<string> programmingLanguages)// when we create a Developer, this constructo is called
+            : base(fullName, age, address, phoneNumber)
+        {
+            ProjectName = projectName;
+            YearsOfExperience = yearsOfExperience;
+            ProgrammingLanguages = programmingLanguages == null ? new List<string>() : programmingLanguages;
+        }
+
+        // this method must be present because it an abstract method in the parent class(Person)
+        public override string GetProfessionalInfo()
+        {
+            return $"{FullName} works as a deloper for {YearsOfExperience}. {FullName} works on {ProjectName}";
+        }
+    }
+}
--- C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces/Program.cs
+++ C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AbstractClassesAndInterfaces.Domain.Models;
+
+Developer firstDeveloper = new Developer("Petko P", 30, "Some address", 1234567890,
+    "Qinshift", 5, new List<string> { "C#", "Java" });
+
+firstDeveloper.PrintInfo(); // we call the implementation from Person class
+Console.WriteLine(firstDeveloper.GetProfessionalInfo()); // we call the implementation from Developer class
+
+//Person person = new Person(); //ERROR -> abstract class cannot be instantiated
