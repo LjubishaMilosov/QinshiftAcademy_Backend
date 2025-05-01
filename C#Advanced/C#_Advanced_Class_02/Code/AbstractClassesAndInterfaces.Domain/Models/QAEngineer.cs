01b593c01ef97e65caf687ddf12a132828aa2a9f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-01 18:14:06 +0800

    QAEngineer class

---
+++ C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces.Domain/Models/QAEngineer.cs
@@ -0,0 +1,31 @@
+﻿
+namespace AbstractClassesAndInterfaces.Domain.Models
+{
+    public class QAEngineer : Person
+    {
+        public int NumberOfProjects { get; set; }
+        public List<string> TestingFrameworks { get; set; }
+
+        public QAEngineer(string fullName, int age, string address, long phoneNumber,
+            int numberOfProjects, List<string> frameworks)
+            : base(fullName, age, address, phoneNumber)
+            {
+            NumberOfProjects = numberOfProjects;
+            TestingFrameworks = frameworks == null ? new List<string>() : frameworks;
+        }
+
+        public override string GetProfessionalInfo()
+        {
+           string info = $"{FullName} works on {NumberOfProjects} projects. ";
+            if( TestingFrameworks.Count > 0)
+            {
+                info += $"{FullName} is familiar with the following testing frameworks: ";
+                foreach (var framework in TestingFrameworks)
+                {
+                    info += $"{framework} \n ";
+                }
+            }
+            return info;
+        }
+    }
+}
