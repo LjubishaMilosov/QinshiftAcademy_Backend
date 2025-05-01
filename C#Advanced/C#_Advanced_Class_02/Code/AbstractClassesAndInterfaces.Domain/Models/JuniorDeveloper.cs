7a5fd86b93428a5a9009c293d7f88a6493c35e61
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-01 17:24:59 +0800

    AbstractClassesAndInterfaces-juniorDeveloper class

---
+++ C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces.Domain/Models/JuniorDeveloper.cs
@@ -0,0 +1,16 @@
+﻿
+namespace AbstractClassesAndInterfaces.Domain.Models
+{
+   public  class JuniorDeveloper : Developer
+    {
+        public bool IsGraduated { get; set; }
+        public JuniorDeveloper() { }
+
+        public JuniorDeveloper(string fullName, int age, string address, long phoneNumber,
+            string projectName, int yearsOfExperience, List<string> programmingLanguages, bool isGraduated)
+            : base(fullName, age, address, phoneNumber, projectName, yearsOfExperience, programmingLanguages)
+        {
+            IsGraduated = isGraduated;
+        }
+    }
+}
--- C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces/Program.cs
+++ C#Advanced/C#_Advanced_Class_02/Code/AbstractClassesAndInterfaces/Program.cs
@@ -7,3 +7,8 @@ firstDeveloper.PrintInfo(); // we call the implementation from Person class
 Console.WriteLine(firstDeveloper.GetProfessionalInfo()); // we call the implementation from Developer class
 
 //Person person = new Person(); //ERROR -> abstract class cannot be instantiated
+
+JuniorDeveloper junior = new JuniorDeveloper("Nikola P", 30, "Some address2", 1234567890,
+    "Qinshift", 5, null, false);
+Console.WriteLine(junior.GetProfessionalInfo()); // we call the implementation from Developer class
+junior.PrintInfo(); // we call the implementation from Person class
