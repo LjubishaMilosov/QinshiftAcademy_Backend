50ae4ed0e981dc53ed1fe51ef42cd81fe6f69424
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-02 18:36:05 +0800

    Exercise_02 student class

--- C#Advanced/C#_Advanced_Class_02/Code/Exercise_01.Domain/Models/Student.cs
+++ C#Advanced/C#_Advanced_Class_02/Code/Exercise_01.Domain/Models/Student.cs
@@ -1,12 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Exercise_01.Domain.Models
 {
-    class Student
+    public class Student : User
     {
+        public List<int> Grades { get; set; }
+
+        public Student(int id, string name, string username, string password, List<int> grades)
+            :base (id, name, username, password)
+        {
+            Grades = grades != null ? grades : new List<int>();
+        }
+
+        public override void PrintUser()
+        {
+            Console.WriteLine($"Student with id {Id} name {Name} and username {Username} has the following grades: ");
+            foreach(int grade in Grades)
+            {
+                Console.WriteLine($"{grade} \n");
+            }
+        }
     }
 }
