0813890e0dc635063c1c6ec427e27d6db1a2a591
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-02 18:15:34 +0800

    Exercise-01 user class

---
+++ C#Advanced/C#_Advanced_Class_02/Code/Exercise_01.Domain/Models/User.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Exercise_01.Domain.Models
+{
+    public abstract class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(int id, string name, string username, string password)
+        {
+            Id = id;
+            Name = name;
+            Username = username;
+            Password = password;
+        }
+    }
+}
