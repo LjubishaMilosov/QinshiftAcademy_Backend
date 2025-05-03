b1262d974fec193deebfb5fda4cb5fd5b5979663
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-04 01:19:45 +0800

    Teacher class and ITeacher interface implementation

--- C#Advanced/C#_Advanced_Class_02/Code/Exercise_01.Domain/Interfaces/ITeacher.cs
+++ C#Advanced/C#_Advanced_Class_02/Code/Exercise_01.Domain/Interfaces/ITeacher.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace Exercise_01.Domain.Interfaces
 {
-    interface ITeacher
+    public interface ITeacher
     {
+        void PrintSubject();
     }
 }
