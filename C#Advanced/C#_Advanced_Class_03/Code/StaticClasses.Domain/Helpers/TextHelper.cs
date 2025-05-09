1b13dc13ca10285e706554aea6e946f07b7c94fc
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-10 01:08:14 +0800

    static class Texthelper

---
+++ C#Advanced/C#_Advanced_Class_03/Code/StaticClasses.Domain/Helpers/TextHelper.cs
@@ -0,0 +1,18 @@
+﻿namespace StaticClasses.Domain.Helpers
+{
+    public static class TextHelper
+    {
+        public static int ValidateInput(string input)
+        {
+            bool success = int.TryParse(input, out int choice);
+            if (success)
+            {
+                return choice;
+            }
+            else
+            {
+                return - 1;
+            }
+        }
+    }
+}
