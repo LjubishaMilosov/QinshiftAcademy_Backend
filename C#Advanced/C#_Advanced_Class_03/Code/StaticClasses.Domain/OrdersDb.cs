676bbf78c23d2d06f54cc92e1b5980e8841293fa
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-10 00:17:15 +0800

    static class OdresDb

---
+++ C#Advanced/C#_Advanced_Class_03/Code/StaticClasses.Domain/OrdersDb.cs
@@ -0,0 +1,54 @@
+﻿using StaticClasses.Domain.Enums;
+using StaticClasses.Domain.Models;
+namespace StaticClasses.Domain
+{
+    // This static class simulates a database for orders
+    // It is not a real database, but it is used to demonstrate the concept of static classes
+    // can be accessed from anywhere 
+    // and it is used to store data that is shared across the application
+    // It is not a good practice to use static classes for data storage
+    // but it is used here to demonstrate the concept of static classes
+    // and how they can be used to store data that is shared across the application
+   
+    public static class OrdersDb
+    {
+        // these list simulate a real data base
+        // all members need to be static
+        public static List<Order> Orders { get; set; }
+        public static List<User> Users { get; set; }
+        // static constructor, all members  must be static
+        static OrdersDb()
+        {
+            // initialize the list
+            Orders = new List<Order>()
+            {
+                new Order(1, "book of books", "best book ever ", OrderStatusEnum.Delivered),
+                new Order(2, "keyboard", "mechanical", OrderStatusEnum.DeliveryInProcess),
+                new Order(3, "shoes", "waterproof", OrderStatusEnum.DeliveryInProcess),
+                new Order(4, "set of pens", "ordinary blue pens", OrderStatusEnum.Processing),
+                new Order(5, "sticky notes", "yellow", OrderStatusEnum.Problem)
+            };
+            Users = new List<User>()
+            {
+                new User(10, "PetkoP", "Adres1"),
+                new User(11, "StefanS", "Adres2")
+            };
+            // add orders to users
+            Users[0].Orders.Add(Orders[0]);
+            Users[0].Orders.Add(Orders[1]);
+            Users[0].Orders.Add(Orders[2]);
+            Users[1].Orders.Add(Orders[3]);
+            Users[1].Orders.Add(Orders[4]);
+        }
+        // this method is used to print all orders, must be static
+        public static void PrintUsers()
+        {
+            int i = 1;
+            foreach (User user in Users)
+            {
+                Console.WriteLine($"User {i}: {user.Username} \n");
+                i++;
+            }
+        }
+    }
+}
--- C#Advanced/C#_Advanced_Class_03/Code/StaticClasses/Program.cs
+++ C#Advanced/C#_Advanced_Class_03/Code/StaticClasses/Program.cs
@@ -1,10 +1,17 @@
-﻿using StaticClasses.Domain.Enums;
+﻿using StaticClasses.Domain;
+using StaticClasses.Domain.Enums;
 using StaticClasses.Domain.Models;
 
-Order order = new Order(1, "First order", "Our first order", OrderStatusEnum.Created);
+Order order = new Order(0, "First order", "Our first order", OrderStatusEnum.Created);
 Console.WriteLine(order.Description); // we access the non-static member using the object (instance) of the class
 order.PrintTitle(); // we access the non-static member using the object (instance) of the class
 
 //order.IsValid(); // this will not work because IsValid is a static method and we are trying to access it using
                  // the object (instance) of the class
 Console.WriteLine(Order.IsValid(order)); // we access the static member using the class name
+
+Console.WriteLine("Welcome to our ordering app");
+Console.WriteLine("Please choose the number of your user:");
+
+OrdersDb.PrintUsers(); // we access the static member using the class name
+string input = Console.ReadLine();
