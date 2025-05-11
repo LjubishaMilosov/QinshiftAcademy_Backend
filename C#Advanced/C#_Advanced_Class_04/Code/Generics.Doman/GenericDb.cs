d6a1769e9764ff8c227a239d9fbd5add3e33a62c
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-11 22:41:55 +0800

    generics db

---
+++ C#Advanced/C#_Advanced_Class_04/Code/Generics.Doman/GenericDb.cs
@@ -0,0 +1,25 @@
+﻿using Generics.Doman.Models;
+
+namespace Generics.Doman
+{
+    //GenericDb can work with any type but that type has to be eithr BaseEntity or it has to inherit from BaseEntity
+    public class GenericDb<T> where T : BaseEntity
+    {
+        private List<T> items;
+        public GenericDb()
+        {
+            items = new List<T>();
+        }
+
+        public void PrintAll()
+        {
+            foreach (T item in items)
+            {
+                // we can call GetInfo because T MUST inherit from BaseEntity, which means that the item has the method GetInfo
+                Console.WriteLine(item.GetInfo());
+            }
+        }
+
+    }
+    
+}
