d300d950d6e0afb4bc16a82f607444f8b7317363
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-13 04:42:59 +0800

    Class_04_Exercise_01 Fish class

--- C#Advanced/C#_Advanced_Class_05/Code/Class_04_Exercise_01.Domain/Models/Fish.cs
+++ C#Advanced/C#_Advanced_Class_05/Code/Class_04_Exercise_01.Domain/Models/Fish.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Class_04_Exercise_01.Domain.Models
 {
-    class Fish
+    public class Fish : Pet
     {
+        public string Color { get; set; }
+        public int Size { get; set; }
+        public Fish(string name, string age, string color, int size)
+            : base(name, age, PetTypeEnum.Fish)
+        {
+            Color = color;
+            Size = size;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"{Name} is of type {Type.ToString()} aged {Age} and is of color  {Color} and size {Size}");
+        }
     }
 }
