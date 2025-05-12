2e8af8b78319d46088e2f56e11495233cac99e5d
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-13 04:39:45 +0800

    Class_04_Exercise_01 Catclass

--- C#Advanced/C#_Advanced_Class_05/Code/Class_04_Exercise_01.Domain/Models/Cat.cs
+++ C#Advanced/C#_Advanced_Class_05/Code/Class_04_Exercise_01.Domain/Models/Cat.cs
@@ -2,7 +2,28 @@
 namespace Class_04_Exercise_01.Domain.Models
 {
 
-    public class Cat
+    public class Cat : Pet
     {
+        public bool IsLazy { get; set; }
+        public int LivesLeft { get; set; }
+
+        public Cat(string name, string age, bool isLazy, int livesLeft)
+            : base(name, age, PetTypeEnum.Cat)
+        {
+            IsLazy = isLazy;
+            LivesLeft = livesLeft;
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"{Name} is of type {Type.ToString()} ageed {Age} and lives left {LivesLeft}");
+            if(IsLazy)
+            {
+                Console.WriteLine($"{Name} is lazy");
+            }
+            else
+            {
+                Console.WriteLine($"{Name} is not lazy");
+            }
+        }
     }
 }
