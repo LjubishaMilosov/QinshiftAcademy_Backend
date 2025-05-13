a9460440bc7158c2e315c418906a156116713b44
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-14 00:31:19 +0800

    IVideoTraing interface

---
+++ C#Advanced/C#_Advanced_Class_06/Code/TryBeingFit.Domain/Iterfaces/IVideoTraining.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryBeingFit.Domain.Iterfaces
+{
+    public interface IVideoTraining
+    {
+        string CheckRating();
+    }
+}
