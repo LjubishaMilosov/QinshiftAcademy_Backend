fc7a49341cd039a1bc9e96f5a3861c4698924fde
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-17 04:06:12 +0800

    class7 students class

---
+++ C#Advanced/C#_Advanced_Class_07/Code/AdvancedLINQ.Domain/Models/Students.cs
@@ -0,0 +1,27 @@
+﻿namespace AdvancedLINQ.Domain.Models
+{
+    public class Students : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsPartTime { get; set; }
+        public List<Subject> Subjects { get; set; }
+        public Students() 
+        {
+            Subjects = new List<Subject>();
+        }
+        public Students(string firstName, string lastName, int age, bool isPartTime)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            IsPartTime = isPartTime;
+            Subjects = new List<Subject>();
+        }
+        public override void PrintInfo()
+        {
+            Console.WriteLine($"{FirstName} {LastName} aged {Age} attends {Subjects.Count}");
+        }
+    }
+}
