c5fc849c21e21f4ee106ec4579645ae958bb9be2
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-29 22:36:13 +0800

    working with file system

--- C#Advanced/C#_Advanced_Class_10/Code/WorkingWithFileSystem/Program.cs
+++ C#Advanced/C#_Advanced_Class_10/Code/WorkingWithFileSystem/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-string currentDirectoy = Directory.GetCurrentDirectory();
+﻿string currentDirectoy = Directory.GetCurrentDirectory();
 Console.WriteLine($"Current Directory: {currentDirectoy}");
 
 string appPath = @"..\..\..\";  // relative path to the console app folder
@@ -9,7 +7,8 @@ string appPath = @"..\..\..\";  // relative path to the console app folder
 bool myFolderExist = Directory.Exists(appPath + "myFolder");
 Console.WriteLine($"myFolder Exist: {myFolderExist}");
 
-string newFolderPath = appPath = "newFolder";
+
+string newFolderPath = appPath + "newFolder";
 if (!Directory.Exists(newFolderPath)) // if this folder does not exist at this location(this path)
 {
     Directory.CreateDirectory(newFolderPath); // here we send the path where we want to create the new directory(folder)
@@ -24,23 +23,23 @@ if (Directory.Exists(newFolderPath)) // always check if folder exists
     Console.WriteLine($"Successfully deleted new folder: {newFolderPath}");
 }
 
-//working with files
+////working with files
 
 if (!Directory.Exists(appPath + "myFolder"))
 {
     Directory.CreateDirectory(appPath + "myFolder");
 }
 
-// Check if a file test.txt exists in the myFolder directory
+//// Check if a file test.txt exists in the myFolder directory
 bool testtxtExists = File.Exists(appPath + @"myFolder/test.txt");
 Console.WriteLine(testtxtExists); // false
 
-if(!testtxtExists)
+if (!testtxtExists)
 {
     File.Create(appPath + @"myFolder\test.txt").Close(); // Create returns a FileStream that we need to close
 }
 
-if(File.Exists(appPath + @"myFolder/test.txt"))
+if (File.Exists(appPath + @"myFolder/test.txt"))  //check if the file was successfully created
 {
     // overrite the content of the file
     File.WriteAllText(appPath + @"myFolder\test.txt", "Hello G6!"); // write to the file
@@ -49,22 +48,22 @@ if(File.Exists(appPath + @"myFolder/test.txt"))
 if (File.Exists(appPath + @"myFolder/test.txt"))
 {
     // overrite the content of the file
-    File.WriteAllText(appPath + @"myFolder\test.txt", "Another text written in txt"); 
+    File.WriteAllText(appPath + @"myFolder\test.txt", "Another text written in txt");
 }
 
 if (File.Exists(appPath + @"myFolder/test.txt"))
 {
     // this will append the text to the content of test.txt
-    File.AppendAllText(appPath + @"myFolder\test.txt", Environment.NewLine + "Goodbye G6"); 
+    File.AppendAllText(appPath + @"myFolder\test.txt", Environment.NewLine + "Goodbye G6");
 }
 
 if (File.Exists(appPath + @"myFolder/test.txt"))
 {
-   
+
     File.AppendAllText(appPath + @"myFolder\test.txt", Environment.NewLine + "See you next Tuesday");
 }
 
-if(File.Exists(appPath + @"myFolder/test.txt"))
+if (File.Exists(appPath + @"myFolder/test.txt"))
 {
     // read the content of the file
     string fileContent = File.ReadAllText(appPath + @"myFolder\test.txt");
