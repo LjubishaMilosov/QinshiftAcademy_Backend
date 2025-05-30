d4a1eeed77e8e6840c167ce09404048282b22ad3
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-05-30 19:48:15 +0800

    loggerService

---
+++ C#Advanced/C#_Advanced_Class_12/Code/SerializatioAndDeserializaion/CustomReaderWriter.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace SerializationAndDeserializaion
+{
+    public class CustomReaderWriter
+    {
+        public string ReadFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
