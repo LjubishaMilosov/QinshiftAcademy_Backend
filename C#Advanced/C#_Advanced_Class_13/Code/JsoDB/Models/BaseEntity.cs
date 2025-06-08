67a5a0f1f7a3eb82232cc8f2021161b5ebbc562e
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-09 01:46:36 +0800

    jsonDB models

---
+++ C#Advanced/C#_Advanced_Class_13/Code/JsoDB/Models/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace JsonDB.Models
+{
+    public  abstract class BaseEntity
+    {
+        public int Iint { get; set; }
+        public abstract string GetInfo();
+    }
+}
---
+++ C#Advanced/C#_Advanced_Class_13/Code/JsoDB/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿namespace JsonDB.Models
+{
+    public class Student : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public override string GetInfo()
+        {
+            return $" {FirstName} {LastName} - {Age}";
+        }
+    }
+
+}
