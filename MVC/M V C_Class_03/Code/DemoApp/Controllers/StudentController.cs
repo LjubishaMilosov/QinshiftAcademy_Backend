24b664ad01c2e25bd7037d15a4869c7a19a8e708
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-19 21:53:12 +0800

    student controller- bad prectice example

---
+++ MVC/M V C_Class_03/Code/DemoApp/Controllers/StudentController.cs
@@ -0,0 +1,15 @@
+﻿using DemoApp.Database;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoApp.Controllers
+{
+    public class StudentController : Controller
+    {
+        //Bad practice - Avoid accessing the db and using domain models in the controller actions
+        public IActionResult GetAllStudents()
+        {
+            return Json(StaticDb.Students);
+        }
+
+    }
+}
