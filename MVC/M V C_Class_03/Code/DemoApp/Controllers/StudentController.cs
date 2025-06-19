be45e37b162b5b5e76b60df154ed936d24d53e51
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-19 22:11:17 +0800

    student service

--- MVC/M V C_Class_03/Code/DemoApp/Controllers/StudentController.cs
+++ MVC/M V C_Class_03/Code/DemoApp/Controllers/StudentController.cs
@@ -1,14 +1,33 @@
 ﻿using DemoApp.Database;
+using DemoApp.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApp.Controllers
 {
     public class StudentController : Controller
     {
+        private StudentService _studentService;
+
+        public StudentController()
+        {
+            _studentService = new StudentService(); //we need an instance of the service so that we can call the methods 
+        }
+
         //Bad practice - Avoid accessing the db and using domain models in the controller actions
         public IActionResult GetAllStudents()
         {
             return Json(StaticDb.Students);
+        }
+
+        public IActionResult GetStudentById(int id)
+        {
+
+            var student = _studentService.GetStudentById(id); //avoid using domain models in the controller actions
+            if (student != null)
+            {
+                return Json(student);
+            }
+            return NotFound();
         }
 
     }
---
+++ MVC/M V C_Class_03/Code/DemoApp/Services/StudentService.cs
@@ -0,0 +1,14 @@
+﻿using DemoApp.Database;
+using DemoApp.Models.Domain;
+
+namespace DemoApp.Services
+{
+    public class StudentService
+    {
+        public Student GetStudentById(int id)
+        {
+            
+            return StaticDb.Students.FirstOrDefault(s => s.Id == id); //when accessing the db we are working with domain models
+        }
+    }
+}
