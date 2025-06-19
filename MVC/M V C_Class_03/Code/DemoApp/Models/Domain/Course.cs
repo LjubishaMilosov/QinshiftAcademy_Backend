3c1cb98134c991554eee0e4f5004093766ba76e5
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-19 21:41:46 +0800

    domain classes

---
+++ MVC/M V C_Class_03/Code/DemoApp/Models/Domain/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoApp.Models.Domain
+{
+    //this is a domain class,it represents an entity from the real world
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int NumberOfClasses { get; set; }
+    }
+}
---
+++ MVC/M V C_Class_03/Code/DemoApp/Models/Domain/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace DemoApp.Models.Domain
+{
+    //this is a domain class,it represents an entity from the real world
+    // this class will act as a static db, and will be used to connect to db
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Course ActiveCourse { get; set; }
+    }
+}
