9567bb8e8d60bef532ff6d7de6c33798da8a75d2
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-20 22:54:47 +0800

    views -student domain

---
+++ MVC/M V C_Class_04/QInshift.Class4/QInshift.Views/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QInshift.Views.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ MVC/M V C_Class_04/QInshift.Class4/QInshift.Views/Models/ViewModels/StudentCourseViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace QInshift.Views.Models.ViewModels
+{
+    public class StudentCourseViewModel
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+        public string CourseName { get; set; }
+        public int NumberOfClasses { get; set; }
+
+    }
+}
