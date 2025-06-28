720f5fd0f523e3097991eb303eb5e7bcbebaecab
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-28 21:15:32 +0800

    view models

---
+++ MVC/M V C_Class_04/QInshift.Class4/QInshift.Views/Models/ViewModels/StudentInfoViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace QInshift.Views.Models.ViewModels
+{
+    public class StudentInfoViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+    }
+}
