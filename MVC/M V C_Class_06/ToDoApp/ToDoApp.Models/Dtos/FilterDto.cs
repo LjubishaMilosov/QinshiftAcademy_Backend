6ebcda0ae6c34c46499796582cab45402aef0f86
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-06-30 16:05:02 +0800

    dtos

---
+++ MVC/M V C_Class_06/ToDoApp/ToDoApp.Models/Dtos/FilterDto.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel;
+using ToDoApp.Domain;
+
+namespace ToDoApp.Models.Dtos
+{
+    public class FilterDto
+    {
+        // we don't want to send back to the controller a list of domain models, so we create dto
+        public List<CategoryDto> Categories { get; set; }
+        public List<StatusDto> Statuses { get; set; }
+        public int CategoryId { get; set; }
+        public int StatusId { get; set; }
+    }
+}
