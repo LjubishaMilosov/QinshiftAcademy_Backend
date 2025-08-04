c2782a0748ee6e200ab6fd95f8cd4b2e0642dc6f
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-04 19:09:47 +0800

    ef implementation

---
+++ MVC/M V C_Recap_ToDoApp/ToDoApp/ToDoApp.DataAccess/EFImplementation/EFCategoryRepository.cs
@@ -0,0 +1,48 @@
+﻿using ToDoApp.DataAccess.Interfaces;
+using ToDoApp.Domain;
+
+namespace ToDoApp.DataAccess.EFImplementation
+{
+    public class EFCategoryRepository : IRepository<Category>
+    {
+        private readonly ToDoAppDbContext _dbContext;
+
+        public EFCategoryRepository(ToDoAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void Create(Category entity)
+        {
+            _dbContext.Category.Add(entity); // Add the new category entity to the DbSet
+            _dbContext.SaveChanges(); // Save changes to the database
+        }
+
+        public void Delete(int id)
+        {
+            var category = GetById(id);
+            if (category != null)
+            {
+                _dbContext.Category.Remove(category); // Remove the category entity from the DbSet
+                _dbContext.SaveChanges(); // Save changes to the database
+            }
+        }
+
+        public List<Category> GetAll()
+        {
+            return _dbContext.Category
+                .ToList(); // Retrieve all category entities from the database
+        }
+
+        public Category GetById(int id)
+        {
+            return _dbContext.Category
+                .FirstOrDefault(c => c.Id == id); // Retrieve a category entity by its ID
+        }
+
+        public void Update(Category entity)
+        {
+            _dbContext.Category.Update(entity); // Update the category entity in the DbSet
+            _dbContext.SaveChanges(); // Save changes to the database
+        }
+    }
+}
---
+++ MVC/M V C_Recap_ToDoApp/ToDoApp/ToDoApp.DataAccess/EFImplementation/EFStatusRepository.cs
@@ -0,0 +1,50 @@
+﻿using ToDoApp.DataAccess.Interfaces;
+using ToDoApp.Domain;
+
+namespace ToDoApp.DataAccess.EFImplementation
+{
+    public class EFStatusRepository : IRepository<Status>
+    {
+        private readonly ToDoAppDbContext _dbContext;
+
+        public EFStatusRepository(ToDoAppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // CRUD methods for accessing the db
+        public void Create(Status entity)
+        {
+            _dbContext.Status.Add(entity); // Add the new status entity to the DbSet
+            _dbContext.SaveChanges(); // Save changes to the database
+        }
+
+        public void Delete(int id)
+        {
+            var status = GetById(id);
+            if (status != null)
+            {
+                _dbContext.Status.Remove(status); // Remove the status entity from the DbSet
+                _dbContext.SaveChanges(); // Save changes to the database
+            }
+        }
+
+        public List<Status> GetAll()
+        {
+            return _dbContext.Status
+                .ToList(); // Retrieve all status entities from the database
+        }
+
+        public Status GetById(int id)
+        {
+            return _dbContext.Status
+                .FirstOrDefault(s => s.Id == id); // Retrieve a status entity by its ID
+        }
+
+        public void Update(Status entity)
+        {
+            _dbContext.Status.Update(entity);
+            _dbContext.SaveChanges();
+        }
+    }
+}
