44ef7117d09ca17231ff1eb65792a23091e34e26
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-04 19:10:19 +0800

    ef connection string

--- MVC/M V C_Recap_ToDoApp/ToDoApp/ToDoApp.Domain/ToDo.cs
+++ MVC/M V C_Recap_ToDoApp/ToDoApp/ToDoApp.Domain/ToDo.cs
@@ -4,7 +4,9 @@
     {
         public string Description { get; set; }
         public DateTime DueDate { get; set; }
+        public Status Status { get; set; } // we need it here in order to represent the relationship with status
         public int StatusId { get; set; }
+        public Category Category { get; set; }
         public int CategoryId { get; set; }
 
     }
--- MVC/M V C_Recap_ToDoApp/ToDoApp/ToDoApp/Program.cs
+++ MVC/M V C_Recap_ToDoApp/ToDoApp/ToDoApp/Program.cs
@@ -1,4 +1,7 @@
-using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.EntityFrameworkCore;
+using ToDoApp.DataAccess;
+using ToDoApp.DataAccess.EFFImplementation;
+using ToDoApp.DataAccess.EFImplementation;
 using ToDoApp.DataAccess.Implementation;
 using ToDoApp.DataAccess.Interfaces;
 using ToDoApp.Domain;
@@ -10,6 +13,16 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+#region Register DataBase
+//First way : hardcoded connection string
+string connectionString = "Server=.\\SQLEXPRESS;Database=TodoAppDb;Trusted_Connection=True;Integrated Security=true;Encrypt=False;";
+
+//Second way: read connection string from appsettings.json
+//string connectionString = builder.Configuration.GetConnectionString("ToDoAppDbConnection");
+
+builder.Services.AddDbContext<ToDoAppDbContext>(options => options.UseSqlServer(connectionString));
+#endregion
+
 //Here we register dependencies using Dependency Injection
 #region Register Repositories
 //scoped lifetime: a new instance is creted once per cliend requests (if in a HTTP request there are multiple requests for this, one instance will be created and used)
@@ -20,10 +33,15 @@ builder.Services.AddControllersWithViews();
 //this tells the app that anywhere where an instance of IRepository<toDo> is requested, the implem that should be called is ToDoRepository.
 //if we want to change the impl, we only need to specify the new impl here 
 //Transient lifetime: a new instance is created every time it is requested
-builder.Services.AddTransient<IRepository<ToDo>, ToDoRepository>();
 
-builder.Services.AddTransient<IRepository<Category>, CategoryRepository>();
-builder.Services.AddTransient<IRepository<Status>, StatusRepository>();
+//builder.Services.AddTransient<IRepository<ToDo>, ToDoRepository>();
+builder.Services.AddTransient<IRepository<ToDo>, EFToDoRepository>();
+
+//builder.Services.AddTransient<IRepository<Category>, CategoryRepository>();
+builder.Services.AddTransient<IRepository<Category>, EFCategoryRepository>();
+
+//builder.Services.AddTransient<IRepository<Status>, StatusRepository>();
+builder.Services.AddTransient<IRepository<Status>, EFStatusRepository>();
 #endregion
 
 #region Register services
