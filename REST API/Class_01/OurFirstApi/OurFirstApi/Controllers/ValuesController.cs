9a19a8d159a848a45ee752c1155de4180de6f05b
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-08 19:45:34 +0200

    web api class 1

---
+++ REST API/Class_01/OurFirstApi/OurFirstApi/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OurFirstApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // https://localhost:[port]/api/values
+        [HttpGet] //no additional info
+        public IEnumerable<string> GetStrings()
+        {
+            return new List<String> { "value1", "value2" };
+        }
+   
+    [HttpGet("info")]
+    public string GetInfo()
+        {
+            return "This is our values controller";
+        }
+    }
+}
+
