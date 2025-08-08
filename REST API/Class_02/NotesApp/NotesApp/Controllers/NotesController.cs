fbb9fc2dfee60456e8d1118b9b1cad162fd8d305
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-08 19:46:41 +0200

    rest api class 2

---
+++ REST API/Class_02/NotesApp/NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesApp.Controllers
+{
+    [Route("api/[controller]")]   // http://localhost:[port]/api/notes
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<string>> Get()
+        {
+            //return StatusCode(StatusCodes.Status200OK, StaticDb.SimpleNotes);
+            return Ok(StaticDb.SimpleNotes);
+        }
+
+        [HttpGet("{index}")]   // http://localhost:[port]/api/notes/1
+
+        public ActionResult<string> GetByIndex(int index)
+        {
+            try
+            {
+                //validations
+                if (index < 0)
+                {
+                    //return StatusCode(StatusCodes.Status400BadRequest, "The index has negative value");
+                    return BadRequest("The index has negative value");
+                }
+                if (index >= StaticDb.SimpleNotes.Count)
+                {
+                    return StatusCode(StatusCodes.Status404NotFound, $"There is no resource on index {index}");
+                }
+                //throw new Exception("Test exception for status 500");
+
+                return Ok(StaticDb.SimpleNotes[index]);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error occured.Please contact your administrator.");
+            }
+        }
+
+        [HttpGet("{noteId}/{userId}")]
+        public ActionResult<string> GetNoteByIdAndUserId(int noteId, int userId)
+        {
+            try
+            {
+                if (noteId < 0 || userId < 0)
+                {
+                    return BadRequest("The ids cannot have negative values");
+                }
+                return Ok($"Returning note with id {noteId} for user with id {userId}");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
+
---
+++ REST API/Class_02/NotesApp/NotesApp/Controllers/ValuesController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotesApp.Controllers
+{
+    [Route("api/[controller]")] // http://localhost:[port]/ap/values
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]   // NO ADDITIONAL INFO NEEDED, DEFAULT GET METHOD
+                    // http://localhost:[port]/api/values
+        public List<string> Get()
+        {
+            return new List<string>() { "Value1", "Value2" };
+        }
+
+        //[HttpGet]    // ERROR - has same http method and same address
+        //             // http://localhost:[port]/api/values
+        //public int GetNumber()
+        //{
+        //    return 1;
+        //}
+
+        [HttpGet("number")]
+
+        public int GetNumber()
+        {
+            return 1;
+        }
+
+
+        [HttpPost("number")]
+
+        public string GetString()
+        {
+            return "Hello";
+        }
+
+        [HttpGet("details/{id}")]
+        public int GetId(int id) // http://localhost:[port]/api/values/details/1
+        {
+            return id;
+        }
+
+        [HttpGet("{id}")]
+        public int GetIdFirstNumber(int id) // http://localhost:[port]/api/values/details/1
+        {
+            return id;
+        }
+
+        [HttpGet("userId/{userId}/bookId/{bookId}")]  // http://localhost:[port]/api/values/userId/1/bookId/2
+        public string GetUserAndBook(int userId, int bookId)
+        {
+            return $"User: {userId}, Book: {bookId}";
+        }
+        [HttpGet("{number}/movies")] // http://localhost:[port]/api/values/1/movies
+        public List<string> GetMovies()
+        {
+                       return new List<string>() { "Deadpool", "Joker" };
+        }
+    }
+}
