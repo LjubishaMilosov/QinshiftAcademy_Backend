b0302fe0c6c2fd45523cdb6e977105b170e0402c
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-10 04:29:26 +0800

    cast model class

--- MVC/VideoRentalOnlineStore-Homework/VideoRentalOnlineStore/VideoRentalOnlineStore/Models/Cast.cs
+++ MVC/VideoRentalOnlineStore-Homework/VideoRentalOnlineStore/VideoRentalOnlineStore/Models/Cast.cs
@@ -2,5 +2,9 @@
 {
     public class Cast
     {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public string Name { get; set; }
+        public Part Part { get; set; }
     }
 }
