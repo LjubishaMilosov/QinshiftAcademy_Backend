592b1ba92fc0e43b9cc96593d7ca94393ac48650
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-10 04:30:02 +0800

    rental model class

--- MVC/VideoRentalOnlineStore-Homework/VideoRentalOnlineStore/VideoRentalOnlineStore/Models/Rental.cs
+++ MVC/VideoRentalOnlineStore-Homework/VideoRentalOnlineStore/VideoRentalOnlineStore/Models/Rental.cs
@@ -2,5 +2,10 @@
 {
     public class Rental
     {
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int UserId { get; set; }
+        public DateTime RentedOn { get; set; }
+        public DateTime? ReturnedOn { get; set; }
     }
 }
