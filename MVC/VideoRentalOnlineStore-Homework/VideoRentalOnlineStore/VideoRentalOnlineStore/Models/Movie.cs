a50f118da8d77491f62687a77f9dd005253429b4
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-10 04:28:51 +0800

    movie model class

--- MVC/VideoRentalOnlineStore-Homework/VideoRentalOnlineStore/VideoRentalOnlineStore/Models/Movie.cs
+++ MVC/VideoRentalOnlineStore-Homework/VideoRentalOnlineStore/VideoRentalOnlineStore/Models/Movie.cs
@@ -2,5 +2,14 @@
 {
     public class Movie
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public Language Language { get; set; }
+        public bool IsAvailable { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public TimeSpan Length { get; set; }
+        public int AgeRestriction { get; set; }
+        public int Quantity { get; set; }
     }
 }
