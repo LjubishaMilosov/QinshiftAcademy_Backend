a0b49d70d012f3a1270aeb33300f1a1fdb9e8748
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-16 18:20:49 +0800

    models updated

---
+++ REST API/class_04/NotesAppExtended/NotesAppExtended/Models/DTOs/NoteDto.cs
@@ -0,0 +1,12 @@
+﻿using NotesAppExtended.Models.Enums;
+
+namespace NotesAppExtended.Models.DTOs
+{
+    public class NoteDto
+    {
+        public string Text { get; set; } 
+        public string UserName { get; set; }
+        public PriorityEnum Priority { get; set; }
+        public List<string> Tags { get; set; }
+    }
+}
