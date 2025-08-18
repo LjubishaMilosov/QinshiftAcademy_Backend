868a37d57fa156949829aad800145bd2d1182dee
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-18 23:35:38 +0800

    moviesApp getAll method

--- REST API/class_05/MoviesApp/MoviesApp/Controllers/MoviesController.cs
+++ REST API/class_05/MoviesApp/MoviesApp/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using MoviesApp.Models.DTOs;
 
 namespace MoviesApp.Controllers
 {
@@ -7,5 +8,32 @@ namespace MoviesApp.Controllers
     [ApiController]
     public class MoviesController : ControllerBase
     {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            try
+            {
+                var moviesDb = StaticDb.Movies;
+                if(moviesDb == null)
+                {
+                    return Ok(new List<MovieDto>()); //we haven't added any movies yet - not an error, just an empty collection
+                }
+                var moviesDto = moviesDb.Select(m => new MovieDto  
+                // sakame da se izbere novo movieDto za sekoj eden item kojsto
+                // go imame tuka pri sto title ke bide m.Title itn..
+                // we need to map the data from the db(movie domain class) into movieDto
+                {
+                    Title = m.Title,
+                    Description = m.Description,
+                    Year = m.Year,
+                    Genre = m.Genre
+                }).ToList();
+                return Ok(moviesDto);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
     }
 }
