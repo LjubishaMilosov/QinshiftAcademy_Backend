b50b8487f39f03274680d86411185cc305bf54ea
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-19 02:36:49 +0800

    movieApp GetByQueryStringId method

--- REST API/class_05/MoviesApp/MoviesApp/Controllers/MoviesController.cs
+++ REST API/class_05/MoviesApp/MoviesApp/Controllers/MoviesController.cs
@@ -36,7 +36,8 @@ namespace MoviesApp.Controllers
             }
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}")]   // we must send the path param because it part of the basic route - it differences this route from the othr routes
+        // if we don't send the path param - then this endpoint will not be hit (it will go to the route for GetAll)
         public ActionResult<MovieDto> GetByPathId(int id)
         {
             try
@@ -60,6 +61,41 @@ namespace MoviesApp.Controllers
                 };
                 return Ok(movieDto);
 
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {ex.Message}");
+            }
+        }
+
+        // it is totally valid not to send a query param - because a query param is notpart of the basic route, but it is added to the basic route - we can still hit this endpoint without this param
+        [HttpGet("queryString")]  // localhost:[port]/api/movies/queryString - this is the route witout the query string part - this is the basic route/url for this method
+                                         // localhost:[port]/api/movies/queryString?id=1 - we can add to the basic route for this method a query string where we can send the params
+        public ActionResult<MovieDto> GetByQueryStringId(int? id)
+        {
+            try
+            {
+                if(id == null)
+                {
+                    return BadRequest("Bad request, the id cannot be null.");
+                }
+                if(id <= 0)
+                {
+                    return BadRequest("Bad request, the id cannot be a negative number.");
+                }
+                var movieDb = StaticDb.Movies.FirstOrDefault(m => m.Id == id);
+                if(movieDb == null)
+                {
+                    return NotFound($"Movie with id {id} not found.");
+                }
+                var movieDto = new MovieDto 
+                { 
+                    Description = movieDb.Description, 
+                    Title = movieDb.Title, 
+                    Year = movieDb.Year, 
+                    Genre = movieDb.Genre 
+                };
+                return Ok(movieDto);
             }
             catch (Exception ex)
             {
