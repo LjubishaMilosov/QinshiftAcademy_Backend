6058c06b325c615cb604cefa395bdea5c28156ec
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-24 01:21:30 +0800

    NotesAppDADbContext

---
+++ REST API/class07/NotesAppDataAnnotations/NotesAppDA.DataAccess/NotesAppDADbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAppDA.Domain;
+
+namespace NotesAppDA.DataAccess
+{
+    public class NotesAppDADbContext : DbContext
+    {
+        public NotesAppDADbContext(DbContextOptions options) : base(options) { }
+
+        // DbSet represents a collection of entities of a specific type that can be queried from the database
+        // and also used to perform CRUD operations on those entities.
+        // Each DbSet property corresponds to a table in the database.
+        // In this case, we have two DbSet properties: Notes and Users.
+        // 
+
+        public DbSet<Note> Notes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+    }
+}
