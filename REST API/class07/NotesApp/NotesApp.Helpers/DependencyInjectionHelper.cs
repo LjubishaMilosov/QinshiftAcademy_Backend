e464954e94353d1cffc99be7c85d555926c85c10
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-25 03:18:01 +0800

    migration and database update

--- REST API/class07/NotesApp/NotesApp.Helpers/DependencyInjectionHelper.cs
+++ REST API/class07/NotesApp/NotesApp.Helpers/DependencyInjectionHelper.cs
@@ -2,13 +2,14 @@
 using Microsoft.Extensions.DependencyInjection;
 using NotesApp.DataAccess;
 
+
 namespace NotesApp.Helpers
 {
     public static class DependencyInjectionHelper
     {
         public static void InjectDbContext(IServiceCollection services)
-        { 
-            services.AddDbContext<NotesAppDbContext>((x => x.UseSqlServer("Server=.\\SQLExpress;Database=AANotesApp;Trusted_Connection=True;TrustServerCertificate=True"));
+        {
+            services.AddDbContext<NotesAppDbContext>(x => x.UseSqlServer("Server=.\\SQLExpress;Database=AANotesApp;Trusted_Connection=True;TrustServerCertificate=True"));
         }
     }
 }
