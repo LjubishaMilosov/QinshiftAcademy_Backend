d936aede88d1a297f005b50ebce3897d5d6f0fcf
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-29 01:52:29 +0800

    NoteService-GetAll & GetById

---
+++ REST API/class07/NotesApp/NotesApp.Services/Implementation/NoteService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using NotesApp.DataAccess.Interfaces;
+using NotesApp.Domain.Models;
+using NotesApp.DTOs;
+using NotesApp.Services.Interfaces;
+
+namespace NotesApp.Services.Implementation
+{
+    public class NoteService : INoteService
+    {
+        public readonly IRepository<Note> _noteRepository;
+        public NoteService(IRepository<Note> noteRepository)
+        {
+            _noteRepository = noteRepository;
+        }
+        public List<NoteDto> GetAllNotes()
+        {
+           var notes = _noteRepository.GetAll();
+              var notesDto = notes.Select(n => new NoteDto
+              {
+                Id = n.Id,
+                Text = n.Text,
+                Tag = n.Tag,
+                Priority = n.Priority,
+                UserFullName = $"{n.User.FirstName} {n.User.LastName}"
+                  //UserFullName = n.User != null ? $"{n.User.FirstName} {n.User.LastName}" : "Unknown User"
+              }).ToList();
+
+             return notesDto;
+        }
+
+        public NoteDto GetNoteById(int id)
+        {
+           var note = _noteRepository.GetById(id);
+              var noteDto = new NoteDto
+              {
+                 Id = note.Id,
+                 Text = note.Text,
+                 Tag = note.Tag,
+                 Priority = note.Priority,
+                 UserFullName = $"{note.User.FirstName} {note.User.LastName}"
+              };
+
+               return noteDto;
+        }
+    }
+}
