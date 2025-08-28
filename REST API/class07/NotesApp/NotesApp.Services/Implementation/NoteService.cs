6eaf060e35f0f35fff492f394c232c5cee124aaf
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-29 02:05:53 +0800

    noteMapper

--- REST API/class07/NotesApp/NotesApp.Services/Implementation/NoteService.cs
+++ REST API/class07/NotesApp/NotesApp.Services/Implementation/NoteService.cs
@@ -2,6 +2,7 @@
 using NotesApp.DataAccess.Interfaces;
 using NotesApp.Domain.Models;
 using NotesApp.DTOs;
+using NotesApp.Mappers;
 using NotesApp.Services.Interfaces;
 
 namespace NotesApp.Services.Implementation
@@ -16,32 +17,38 @@ namespace NotesApp.Services.Implementation
         public List<NoteDto> GetAllNotes()
         {
            var notes = _noteRepository.GetAll();
-              var notesDto = notes.Select(n => new NoteDto
-              {
-                Id = n.Id,
-                Text = n.Text,
-                Tag = n.Tag,
-                Priority = n.Priority,
-                UserFullName = $"{n.User.FirstName} {n.User.LastName}"
-                  //UserFullName = n.User != null ? $"{n.User.FirstName} {n.User.LastName}" : "Unknown User"
-              }).ToList();
+              //var notesDto = notes.Select(n => new NoteDto
+              //{
+              //  Id = n.Id,
+              //  Text = n.Text,
+              //  Tag = n.Tag,
+              //  Priority = n.Priority,
+              //  UserFullName = $"{n.User.FirstName} {n.User.LastName}"
+              //    //UserFullName = n.User != null ? $"{n.User.FirstName} {n.User.LastName}" : "Unknown User"
+              //}).ToList();
 
-             return notesDto;
+            //var notesDto = notes.Select(x => Mappers.NoteMapper.ToDto(x)).ToList();
+            var notesDto = notes.Select(x => x.ToDto()).ToList(); // Extension method usage
+
+            return notesDto;
         }
 
         public NoteDto GetNoteById(int id)
         {
            var note = _noteRepository.GetById(id);
-              var noteDto = new NoteDto
-              {
-                 Id = note.Id,
-                 Text = note.Text,
-                 Tag = note.Tag,
-                 Priority = note.Priority,
-                 UserFullName = $"{note.User.FirstName} {note.User.LastName}"
-              };
+              //var noteDto = new NoteDto
+              //{
+              //   Id = note.Id,
+              //   Text = note.Text,
+              //   Tag = note.Tag,
+              //   Priority = note.Priority,
+              //   UserFullName = $"{note.User.FirstName} {note.User.LastName}"
+              //};
 
-               return noteDto;
+              //var noteDto = Mappers.NoteMapper.ToDto(note);
+              var noteDto = note.ToDto();  // Extension method usage
+
+            return noteDto;
         }
     }
 }
