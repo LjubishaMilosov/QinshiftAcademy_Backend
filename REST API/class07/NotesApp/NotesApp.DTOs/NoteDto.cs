d4bf50ea2e4648aec70d9d474c14b55c2ef79af4
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-29 01:21:11 +0800

    NoteDto

---
+++ REST API/class07/NotesApp/NotesApp.DTOs/NoteDto.cs
@@ -0,0 +1,12 @@
+﻿using NotesApp.Domain.Enums;
+
+namespace NotesApp.DTOs
+{
+    public class NoteDto
+    {
+        public String Text { get; set; }
+        public PriorityEnum Priority { get; set; }
+        public TagEnum Tag { get; set; }
+        public String UserFullName { get; set; }
+    }
+}
