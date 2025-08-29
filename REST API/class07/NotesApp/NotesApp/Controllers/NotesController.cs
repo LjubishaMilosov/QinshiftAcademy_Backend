c9168a27ce932d77c849ce04f2d49e52d266b997
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-08-30 04:52:14 +0800

    GetAll() action

---
+++ REST API/class07/NotesApp/NotesApp/Controllers/NotesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NotesApp.DTOs;
+using NotesApp.Services.Interfaces;
+
+namespace NotesApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+
+        public NotesController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<NoteDto>> GetAll()
+        {
+            try
+            {
+                var result =  Ok(_noteService.GetAllNotes());
+                return result;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
