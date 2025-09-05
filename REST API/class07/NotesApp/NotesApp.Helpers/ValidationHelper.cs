369ddb11c3074deff025640fdc7880b3248a9f07
LjubishaMilosov <ljubisa.milosov@gmail.com>
2025-09-06 02:12:45 +0800

    validation helper

---
+++ REST API/class07/NotesApp/NotesApp.Helpers/ValidationHelper.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+
+namespace NotesApp.Helpers
+{
+    public static class ValidationHelper
+    {
+        public static void ValidateRequiredStringColumnLength(string value, string field, int maxNumOfChar)
+        {
+            if(string.IsNullOrEmpty(value))
+            {
+                throw new DataException($"{field} is required.");
+            }
+            if(value.Length > maxNumOfChar)
+            {
+                throw new DataException($"{field} cannot be longer than {maxNumOfChar} characters.");
+            }
+
+        }
+
+        public static void ValidateColumnLength(string value, string field, int maxNumOfChar)
+        {
+            var length = value == null ? 0 : value.Length;
+            if (value.Length > maxNumOfChar)
+            {
+                throw new DataException($"{field} cannot be longer than {maxNumOfChar} characters.");
+            }
+
+        }
+    }
+}
